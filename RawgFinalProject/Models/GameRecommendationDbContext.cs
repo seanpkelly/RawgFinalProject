2cbefa923ff0e0651e6d5f42ac0470b53806c9b3
justin <justin.d.locke@gmail.com>
2020-09-11 14:35:22 -0400

    added connection to azure db

---
+++ RawgFinalProject/Models/GameRecommendationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace RawgFinalProject.Models
+{
+    public partial class GameRecommendationDbContext : DbContext
+    {
+        public IConfiguration Configuration { get; }
+
+        public GameRecommendationDbContext()
+        {
+        }
+
+        public GameRecommendationDbContext(DbContextOptions<GameRecommendationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("AzureDbConnection"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
