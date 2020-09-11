22f9b154d9616eeefcbf0fd55865181120c0ecef
seanpkelly <yllekpnaes@gmail.com>
2020-09-11 14:53:33 -0400

    set up identity

--- RawgFinalProject/Startup.cs
+++ RawgFinalProject/Startup.cs
@@ -29,8 +29,8 @@ namespace RawgFinalProject
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    Configuration.GetConnectionString("AzureDbConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
