68ea34fc9ed31872568e23e3be104659dc9128ac
CelliniJ <68167256+CelliniJ@users.noreply.github.com>
2020-09-15 16:08:27 -0400

    Anna's ordered dictionary

--- RawgFinalProject/Controllers/GameController.cs
+++ RawgFinalProject/Controllers/GameController.cs
@@ -193,14 +193,8 @@ namespace RawgFinalProject.Controllers
 
             int maxCount = genreCountDictionary.Values.Max();
 
+            genreCountDictionary = (Dictionary<string, int>)genreCountDictionary.OrderByDescending(i => i.Value);
 
-
-            Dictionary<string, int> orderedGenreCount = new Dictionary<string, int>();
-
-            foreach (var item in genreCountDictionary.OrderByDescending(i => i.Value))
-            {
-                orderedGenreCount.Add(item.Key, item.Value);
-            }
 
 
             //We need to get a list of Genres/Tags for each weight level (5, 3, 2, 1, 0) so that we can apply the weighted score to the full database to get recommendations
@@ -217,7 +211,7 @@ namespace RawgFinalProject.Controllers
                 }
             }
 
-            return View(orderedGenreCount);
+            return View(genreCountDictionary);
         }
     }
 }
