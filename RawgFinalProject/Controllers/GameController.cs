bb69f7278e5a339186b220167d1ec8867e7c4f7f
justin <justin.d.locke@gmail.com>
2020-09-21 12:01:25 -0400

    removed comments from consolidation

--- RawgFinalProject/Controllers/GameController.cs
+++ RawgFinalProject/Controllers/GameController.cs
@@ -332,124 +332,22 @@ namespace RawgFinalProject.Controllers
         [Authorize]
         public async Task<List<Dictionary<string,double>>> GenerateWeights()
         {
-            //test genre names
-            //Entire array of genres in API
-            //string[] genres =
-            //    { "Action", "Indie", "Adventure", "RPG", "Strategy", 
-            //    "Shooter", "Casual", "Simulation", "Puzzle", "Arcade", "Platformer", "Racing",
-            //    "Sports", "Massively Multiplayer", "Family", "Fighting", "Board Games", "Educational", "Card" };
-
-            ////test tag names
-            ////Selected array of tags in API
-            //string[] tags = { "Singleplayer", "Multiplayer", "Atmospheric", "Great Soundtrack", "RPG", "Co-op", "Story Rich", "Open World", "cooperative", "First-Person", "Sci-fi", 
-            //    "2D", "Third Person", "FPS", "Horror", "Fantasy", "Comedy", "Sandbox", "Survival", "Exploration", "Stealth", "Tactical", "Pixel Graphics", "Action RPG", "Retro",
-            //    "Space", "Zombies", "Point & Click", "Action-Adventure", "Hack and Slash", "Side Scroller", "Survival Horror", "RTS", "Roguelike", "mmo", "Driving", "Puzzle",
-            //    "MMORPG", "Management", "JRPG" };
-
-            ////creates a prepopulates a dictionary to log how many occurences of each genre appear in users favorites
-            //Dictionary<string, int> genreCountDictionary = new Dictionary<string, int>();
-            //foreach (var g in genres)
-            //{
-            //    genreCountDictionary.Add(g, 0);
-            //}
-
-            ////creates a prepopulates a dictionary to log how many occurences of each tag appear in users favorites
-            //Dictionary<string, int> tagCountDictionary = new Dictionary<string, int>();
-            //foreach (var t in tags)
-            //{
-            //    tagCountDictionary.Add(t, 0);
-            //}
 
             Dictionary<string, int> genreCountDictionary = PopulateGenreDictionary();
             Dictionary<string, int> tagCountDictionary = PopulateTagDictionary();
 
-            //CallIDString method
             string activeUserId = GetActiveUser();
 
-            //Creates list of favorites for the current user
             var favList = await _gameContext.UserFavorite.Where(x => x.UserId == activeUserId).ToListAsync();
 
             List<Result> convertList = await ConvertToResult(favList);
 
-            //List<Result> convertList = new List<Result>();
-
-            //for (int i = 0; i < favList.Count; i++) //8 seconds
-            //{
-            //    convertList.Add(await SearchResultById(favList[i].GameId));
-            //}
-
-
-
-
-            //populates tag and genre dictionaries with the number of occurences of each tag/genre for the game
-
             genreCountDictionary = CountGenreOccurences(convertList, genreCountDictionary);
             tagCountDictionary = CountTagOccurences(convertList, tagCountDictionary);
-
-
-            //foreach (Result result in convertList)
-            //{
-            //    foreach (string key in genreCountDictionary.Keys.ToList())
-            //    {
-            //        for (int i = 0; i < result.genres.Length; i++)
-            //        {
-            //            if (key == result.genres[i].name)
-            //            {
-            //                genreCountDictionary[key] += 1;
-            //            }
-            //        }
-            //    }
-            //    foreach (string key in tagCountDictionary.Keys.ToList())
-            //    {
-            //        for (int i = 0; i < result.tags.Length; i++)
-            //        {
-            //            if (key == result.tags[i].name)
-            //            {
-            //                tagCountDictionary[key] += 1;
-            //            }
-            //        }
-            //    }
-            //}
 
             Dictionary<string, double> weightedGenres = CalculateWeights(genreCountDictionary);
             Dictionary<string, double> weightedTags = CalculateWeights(tagCountDictionary);
 
-            //Variables for sorting by genre.count and applying weights to each genre
-            //int totalGenres = 0;
-            //Dictionary<string, int> orderedGenreCount = new Dictionary<string, int>();
-            //Dictionary<string, double> weightedGenres = new Dictionary<string, double>();
-
-            //foreach (var item in genreCountDictionary.OrderByDescending(i => i.Value))
-            //{
-            //    totalGenres += item.Value;
-            //    orderedGenreCount.Add(item.Key, item.Value);
-            //}
-            //foreach (var g in orderedGenreCount)
-            //{
-            //    double value = Math.Round(((double)g.Value / (double)totalGenres),2);
-            //    weightedGenres.Add(g.Key, value);
-            //}
-
-
-
-
-            ////Variables for sorting by tags.count and applying weights to each tag
-            //int totalTags = 0;
-            //Dictionary<string, int> orderedTagCount = new Dictionary<string, int>();
-            //Dictionary<string, double> weightedTags = new Dictionary<string, double>();
-
-            //foreach (var item in tagCountDictionary.OrderByDescending(i => i.Value))
-            //{
-            //    totalTags += item.Value;
-            //    orderedTagCount.Add(item.Key, item.Value);
-            //}
-            //foreach (var g in orderedTagCount)
-            //{
-            //    double value = Math.Round(((double)g.Value / (double)totalTags), 2);
-            //    weightedTags.Add(g.Key, value);
-            //}
-
-            //List to pass dictionaries to other actions
             List<Dictionary<string, double>> genreAndTagDictionaries = new List<Dictionary<string, double>>();
             genreAndTagDictionaries.Add(weightedGenres);
             genreAndTagDictionaries.Add(weightedTags);
@@ -534,98 +432,14 @@ namespace RawgFinalProject.Controllers
         [Authorize]
         public async Task<IActionResult> GenerateRecommendations()
         {
-            //obtains weighted genre and tag values to apply to game list
             List<Dictionary<string, double>> weights = await GenerateWeights();
-
-            //Changing queries into partial strings to API endpoint
 
             string genreQuery = CreateQuery(weights[0]);
             string tagQuery = CreateQuery(weights[1]);
 
-            //string genreQuery = "";
-            //string tagQuery = "";
-            //foreach (string key in weights[0].Keys.ToList())
-            //{
-            //    if (weights[0][key] != 0)
-            //    {
-            //        genreQuery += key.Replace(" ", "-").ToLower() + ","; 
-            //    }
-            //}
-            //foreach (string key in weights[1].Keys.ToList())
-            //{
-            //    if (weights[1][key] != 0)
-            //    {
-            //        tagQuery += key.Replace(" ", "-").ToLower() + ",";
-            //    }
-            //}
-
-
-
-
-            //This into method?
-
             List<Result> recommendationResultPool = await GenerateResultPool(genreQuery, tagQuery);
 
-
-
-            //SearchResult singlePageResults = new SearchResult();
-            //List<Result> recommendationResultPool = new List<Result>();
-
-            //string activeUserId = GetActiveUser();
-
-
-            //for (int i = 1; i < 5; i++)
-            //{
-            //    singlePageResults = await _gameDAL.GetGameListByGenreAndTag($"genres={genreQuery}&tags={tagQuery}&page={i}");
-            //    //singlePageResults = await _gameDAL.GetGameListByGenreAndTag($"genres=sports&page={i}");
-
-            //    foreach (var result in singlePageResults.results)
-            //    {
-            //        UserFavorite checkForDupes = _gameContext.UserFavorite.Where(f => f.UserId == activeUserId && f.GameId == result.id).FirstOrDefault();
-
-            //        if (checkForDupes == null)
-            //        {
-            //            recommendationResultPool.Add(result);
-            //        }
-            //    }
-            //}
-
-
-            //Calculate score through genres and tags present in Favorites List
-
             List<Result> orderedRecs = GenerateScores(recommendationResultPool, weights);
-
-            //List<Result> gameRecs = new List<Result>();
-            //foreach (Result result in recommendationResultPool)
-            //{
-            //  double genreRecScore = 0;
-            //  double tagRecScore = 0;
-            //  double totalRecScore = 0;
-            //    foreach (Genre genre in result.genres)
-            //    {
-            //        genreRecScore += weights[0][genre.name];
-            //    }
-            //    foreach (Tag tag in result.tags)
-            //    {
-            //        if (weights[1].ContainsKey(tag.name.ToString()))
-            //        {
-            //            tagRecScore += weights[1][tag.name];
-            //        }
-            //    }
-
-            //    totalRecScore = Math.Round((genreRecScore * 7) + (tagRecScore * 3),2);
-            //    result.recommendationScore = totalRecScore;
-            //    gameRecs.Add(result);
-            //}
-
-
-
-            ////Orders recommendations by score
-            //List<Result> orderedRecs = new List<Result>();
-            //foreach (var item in gameRecs.OrderByDescending(i => i.recommendationScore))
-            //{
-            //    orderedRecs.Add(item);
-            //}
 
             return View("GenerateRecommendations", orderedRecs);
         }
